41c59e8eabaf57426e2eaf304892db72df0c9349
JJohnson352 <jjohnson9@ualr.edu>
2022-01-31 16:43:32 +0000

    save

---
+++ 01.05 Previous and Next.py.py
@@ -0,0 +1,9 @@
+X= input("first number: ")
+Y=("second number:1 ")
+Z=("third number:1 ")
+firstnumber = int(X)
+secondnumber = int(Y)
+thirdnumber = int(Z)
+total = firstnumber + secondnumber
+total = firstnumber - thirdnumber 
+print(total)
