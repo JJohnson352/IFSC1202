1e24a9d6bed5b6506500e83bc64cd0b5aefabd01
JJohnson352 <jjohnson9@ualr.edu>
2022-02-02 15:42:14 +0000

    confirm

---
+++  01.07 School Desks.py
@@ -0,0 +1,8 @@
+X= input("first number: ")
+Y= input("second number: ")
+Z= input("third number: ")
+firstnumber = int(X)
+secondnumber = int(Y)
+thirdnumber = int(Z) 
+total = firstnumber / 2 + secondnumber / 2 + thirdnumber / 2 
+print(total)
--- 01.05 Previous and Next.py.py
+++ 01.05 Previous and Next.py.py
@@ -1,10 +1,10 @@
 X= input("first number: ")
-Y=input("second number: ")
+Y= input("second number: ")
 firstnumber = int(X)
 secondnumber = int(Y)
 total = firstnumber + secondnumber
 print(total)
-Z=input("third number: ")
+Z= input("third number: ")
 thirdnumber = int(Z)
 total = firstnumber - thirdnumber
 print(total)
